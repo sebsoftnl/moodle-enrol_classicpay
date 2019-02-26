b1d660e26bfcd4f60fdc0a7bbb24b9d052fbacca
Jeremy van Lier <44089799+jeremyvanlier@users.noreply.github.com>
2019-02-26 14:03:28 +0000

    Removed dependency to the old events library.

--- xchange.php
+++ xchange.php
@@ -28,7 +28,6 @@
  */
 require("../../config.php");
 require_once("lib.php");
-require_once($CFG->libdir . '/eventslib.php');
 require_once($CFG->libdir . '/enrollib.php');
 require_once($CFG->libdir . '/filelib.php');
 
